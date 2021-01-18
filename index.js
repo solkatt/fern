532880809e409eecf171d00facaaf962d4a15c5a
Viktor Ödman <viktor_odman@hotmail.com>
2021-01-18 21:16:18 +0100

    Update

--- index.js
+++ index.js
@@ -44,9 +44,10 @@ db.on('error', console.log.bind(console, 'MongoDB connection error:'));
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static( 'client/build' ));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname+'/client/build/index.html'))
-    });
+    app.get('*', function (req, res) {
+        const index = path.join(__dirname, 'build', 'index.html');
+        res.sendFile(index);
+      });
 }
 
 

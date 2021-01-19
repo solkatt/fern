e4c4d665273a5e0697633b32bf4bfbe52b4a24c7
Viktor Ödman <viktor_odman@hotmail.com>
2021-01-19 09:12:06 +0100

    Update baseURL

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api'
+    baseURL: process.env.baseURL + '/api' || 'http://localhost:3000/api'
 })
 
 export const insertMovie = payload => api.post(`/movie`, payload)

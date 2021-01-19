81535d16418004e90f25a57886b2cd241be62d60
Viktor Ödman <viktor_odman@hotmail.com>
2021-01-19 08:04:18 +0100

    Update

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
     app.get('*', function (req, res) {
-        const index = path.join(__dirname, 'client', 'build', 'index.html');
+        const index = path.resolve(__dirname, 'client', 'build', 'index.html');
         res.sendFile(index);
     });
 }
